3b077e894a4b2d304cf6d21415db7b2cdc606960
caprest <caprest.f@gmail.com>
2017-01-26 02:00:37 +0900

    add 01-05

--- 1.py
+++ 1.py
@@ -40,3 +40,20 @@ a4_list +=[ [i,a4_words[i-1][0:2]] for i in range(1,len(a4_words)) if i not in a
 a4_ans = dict(a4_list)
 print("04: "+str(a4_ans))
 
+#05
+def ngram(string,n):
+    words = [(string[i:i+n],i) for i in range(len(string)-n+1) ]
+    ngram_dict ={}
+    for word in words:
+        if word[0] in ngram_dict:
+            ngram_dict[word[0]].append(word[1])
+        else:
+            ngram_dict[word[0]] = [word[1]]
+
+    return ngram_dict
+a5 = "I am an NLPer"
+dictionary =ngram(a5,2)
+print("05: "+str(dictionary))
+
+
+    
