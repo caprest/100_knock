2191552780020b21dd8b689c6095a00335cb513e
caprest <caprest.f@gmail.com>
2017-02-11 02:25:24 +0900

    added 34,36,37

--- 4.py
+++ 4.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 import re
+import matplotlib.pyplot as plt
+import collections
+import numpy as np
+plt.rcParams['font.family'] = 'MotoyaLMaru'
 
+"""
+mecab neko.txt -o neko.txt.mecab
+to make neko.txt.mecab
+"""
 #For Windows to print utf-8
 import sys
 import io # 追加
@@ -36,7 +44,7 @@ for word in text:
         word_list.append(process_parse(word.strip()))
 
 
-#31 #32 #33
+#31 #32 #33 #34
 verb_list=[]
 verb_base_list=[]
 sahen_list=[]
@@ -47,6 +55,57 @@ for sentence in sentence_list:
             verb_base_list.append(word["base"])
         if (word["pos1"] == "サ変接続") & (word["pos"] == "名詞"):
             sahen_list.append(word["surface"]) 
+'''
 print(verb_list)
 print(verb_base_list)
 print(sahen_list)
+'''
+
+#35
+AsB = []
+for sentence in sentence_list:
+    words=[]
+    for word in sentence:
+        if word["pos"] == "名詞":
+            words.append(word["surface"])
+            if len(words)==3:
+                AsB.append("".join(words))
+                words = [word["surface"]] #AのBのCも一応考慮
+            elif len(words)==2:
+                words = []
+            else:
+                continue
+        if len(words)==1:
+            if word["surface"] == "の":
+                 words.append(word["surface"])
+            else:
+                words=[]
+#print(AsB)
+
+#36 #37
+word_list = [word["base"]  for sent in sentence_list for word in sent]
+noun_list = [word["base"] 
+                for sent in sentence_list
+                    for word in sent
+                        if (word["pos1"] == "一般") & (word["pos"] == "名詞")
+            ]       
+word_ranking_10 = collections.Counter(word_list).most_common(10)
+noun_ranking_10 = collections.Counter(noun_list).most_common(10)
+print(word_ranking_10)
+print(noun_ranking_10)
+fig = plt.figure()
+ax1 = fig.add_subplot(121)
+ax2 = fig.add_subplot(122)
+def ranking_plot(ranking,ax,title):
+    x = np.array([pair[1] for pair in ranking])[::-1]
+    y = np.arange(10)
+    ax.barh(y,x,align = "center")
+    ax.set_yticks(y)
+    ax.set_yticklabels([ str(i+1)+": "+pair[0] for i,pair in enumerate(ranking)][::-1])
+    ax.set_xlabel("頻度[回]")
+    ax.set_ylabel("単語")
+    ax.set_title(title)
+ranking_plot(word_ranking_10,ax1,"ベスト10")
+ranking_plot(noun_ranking_10,ax2,"ベスト10(名詞)")
+plt.tight_layout()
+plt.show()
--- README.md
+++ README.md
@@ -1,12 +1,27 @@
 # 言語処理100本ノック
+##概要
 http://www.cl.ecei.tohoku.ac.jp/nlp100/
 を解いていく
+##環境
+*Anaconda4.2.0
+*MeCab
+*Win10 64bit
+*Unixコマンドが必要な部分はbash on windowsで。
 
 #Mecab
-win10 64bitでの構築に苦労したのでメモ。
-基本は https://github.com/buruzaemon/natto/wiki/64-Bit-Windows に沿ってやればOK。
+##win10 64bitでの構築に苦労したのでメモ。
+基本は 
+https://github.com/buruzaemon/natto/wiki/64-Bit-Windows 　
+にのっとってやればできる。
+https://kiidax.wordpress.com/2015/10/04/msvc-15%E3%81%A7mecab-python-0-996%E3%82%92%E3%83%93%E3%83%AB%E3%83%89/　　
+も参考にした。
+インストーラーを使うとき辞書はUTF-8を使った方が良い。（コマンドプロンプトなら出力がバグるが）　
 pythonのインターフェースとしては,nattoを使ってやれば十分だった。(今回は使わない)
 こちらは
-pip install natto
+　　　　pip install natto
 で入る。
-#
+
+##Mecabの出力について
+表層形\t品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用型,活用形,原形,読み,発音
+の順に並ぶ。
+
